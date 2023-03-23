cc78f3889fb905d8fb1fbba8ccc21c45643e5ae8
Sitou <yehezkielowen0901@gmail.com>
2023-03-23 21:16:56 +0800

    Login Page Polish

--- my-app/src/components/LoginForm.jsx
+++ my-app/src/components/LoginForm.jsx
@@ -1,31 +1,43 @@
-import React from "react";
+import React from 'react';
 
 export default function Form() {
   return (
-    <div className=" text-center w-[695.27px] h-[651.92px] top-[188.54px] left-[372.37px] px-[84px] gap-[31px] rounded-[12px] bg-[#FAFAFA] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.25)] flex flex-col justify-center items-center">
-      <div className="flex flex-col justify-center items-center -mt-[70px]">
-        <div className="flex leading-[209.37px] text-[72px] font-inter font-bold ">
-          <h1 className="text-[#37474F]">Welcome</h1>
-          <h1 className="text-[#FF6593]">!</h1>
+    <div className='max-w-[625px] text-center p-12 md:p-[60px] gap-[32px] rounded-[12px] bg-[#FAFAFA] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.25)] flex flex-col justify-center items-center'>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex text-5xl md:text-[62px] font-inter font-bold '>
+          <h1 className='text-[#37474F]'>Welcome</h1>
+          <h1 className='text-[#FF6593]'>!</h1>
         </div>
-        <p className="font-inter font-normal text-[24px] leading-[29.05px] -mt-[60px]">
+        <p className='font-inter font-normal text-lg md:text-[24px] leading-[29.05px] '>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
           convallis ligula diam, sit amet hendrerit libero sodales quis.
         </p>
       </div>
+      <div className='flex flex-col gap-6 w-full'>
+        <div className='flex flex-col items-start '>
+          <label htmlFor='email' className='mb-2'>
+            Registration Number
+          </label>
+          <input
+            id='email'
+            type='text'
+            placeholder='Email'
+            className='w-full py-6 border-none rounded-xl shadow-lg shadow-[#00000026] font-inter font-normal text-lg md:text-[24px] pl-[22px] placeholder:text-[#37474F40]'
+          />
+        </div>
+        <div className='flex flex-col items-start '>
+          <label htmlFor='email' className='mb-2'>
+            Token
+          </label>
+          <input
+            type='password'
+            placeholder='Password'
+            className='w-full py-6 border-none rounded-xl shadow-lg shadow-[#00000026] font-inter font-normal text-lg md:text-[24px] pl-[22px] placeholder:text-[#37474F40]'
+          />
+        </div>
+      </div>
 
-      <input
-        type="text"
-        placeholder="Email"
-        className="w-[515px] h-[78px] border-[2px] border-[#37474F] font-inter font-normal text-[24px] placeholder:pl-[22px] placeholder:leading-[29.05px] placeholder:text-[#37474F]"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        className="w-[515px] h-[78px] border-[2px] border-[#37474F] font-inter font-normal text-[24px] placeholder:pl-[22px] placeholder:leading-[29.05px] placeholder:text-[#37474F]"
-      />
-
-      <button className="w-[500px] h-[61px] rounded-[25px] bg-[#B55FFE] text-[#FAFAFA] font-semibold text-[24px] leading-[29.05px]">
+      <button className='uppercase w-full py-4 rounded-full bg-[#B55FFE] text-[#FAFAFA] font-semibold text-lg md:text-[24px]'>
         Login
       </button>
     </div>
--- my-app/src/page/Login.jsx
+++ my-app/src/page/Login.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import LoginForm from "../components/LoginForm";
+import React from 'react';
+import LoginForm from '../components/LoginForm';
 export default function Login() {
   return (
-    <body className="w-[1440px] h-[1060px] -top-[5px] bg-[url('./image/Background.svg')] bg-no-repeat flex justify-center items-center">
+    <body className="w-full h-screen bg-[url('./image/Background.svg')] bg-cover bg-no-repeat flex justify-center items-center">
       <LoginForm />
     </body>
   );
