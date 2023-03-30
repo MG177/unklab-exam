1c6194d4597560ad65dc52ed6878cdf87b95ba2e
Sitou <yehezkielowen0901@gmail.com>
2023-03-30 23:06:27 +0800

    Added comment and option radio button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./page/Login";
-import Getstarted from "./page/Getstarted";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './page/Login';
+import Getstarted from './page/Getstarted';
 // import Questions from "./components/Question";
 // import Option from "./components/Option";
-import ScoreCountdown from "./page/ScoreCountdown";
-import Exam from "./page/Exam";
-import Score from "./page/Score";
+import ScoreCountdown from './page/ScoreCountdown';
+import Exam from './page/Exam';
+import Score from './page/Score';
 // import Testing from "./page/Testing";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/started" element={<Getstarted />} />
-        <Route path="/score-countdown" element={<ScoreCountdown />} />
-        <Route path="/exam" element={<Exam />} />
-        <Route path="/score" element={<Score />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/started' element={<Getstarted />} />
+        <Route path='/score-countdown' element={<ScoreCountdown />} />
+        <Route path='/exam/' element={<Exam />} />
+        <Route path='/score' element={<Score />} />
       </Routes>
     </BrowserRouter>
   );
--- my-app/src/page/Score.jsx
+++ my-app/src/page/Score.jsx
@@ -1,41 +1,39 @@
-import React from "react";
-import Header from "../components/Header";
+import React from 'react';
+import Header from '../components/Header';
 // import Cancel from "../image/x.svg";
-import ProgressBar from "../image/progress_bar.svg";
+import ProgressBar from '../image/progress_bar.svg';
 
 export default function Score() {
   return (
-    <div className="relative flex flex-col items-center w-full min-h-screen bg-[#FCF9FF]">
+    <div className='relative flex flex-col items-center w-full min-h-screen bg-[#FCF9FF]'>
       <Header />
-      <p className="mt-[183px] text-black text-6xl font-Nunito font-bold">
+      <p className='mt-[183px] text-black text-6xl font-Nunito font-bold'>
         YOUR SCORE
       </p>
-      <div className="bg-taccent1 w-[29.151875rem] h-[35.6875rem] mt-16 rounded-[37px] flex justify-center items-center">
-        <div className="bg-white w-[26.401875rem] h-[32.9375rem] rounded-[24px] flex flex-col justify-center items-center">
-          <div className="relative flex flex-col justify-center items-center drop-shadow-[2px_3px_7px_rgba(0,0,0,0.15)]">
-            <img src={ProgressBar} alt="" />
-<<<<<<< HEAD
-            <div className="absolute top-[50%] left-[17.5%]">
-              <p className="font-Nunito text-6xl font-bold text-black">
+      <div className='bg-taccent1 w-[29.151875rem] h-[35.6875rem] mt-16 rounded-[37px] flex justify-center items-center'>
+        <div className='bg-white w-[26.401875rem] h-[32.9375rem] rounded-[24px] flex flex-col justify-center items-center'>
+          <div className='relative flex flex-col justify-center items-center drop-shadow-[2px_3px_7px_rgba(0,0,0,0.15)]'>
+            <img src={ProgressBar} alt='' />
+            <div className='absolute flex flex-col justify-center items-center mt-[70px]'>
+              <p className='font-Nunito text-6xl font-bold text-black border-b-2 border-[#98A0A4] pb-3'>
                 75/100
               </p>
-              <p>Passing Grade</p>
-=======
-            <div className="absolute flex flex-col justify-center items-center mt-[70px]">
-              <p className="font-Nunito text-6xl font-bold text-black border-b-2 border-[#98A0A4] pb-3">75/100</p>
-              <p className="font-Roboto text-[17px] text-[#98A0A4] ">Passing Grade</p>
-              <p className="font-Nunito text-[29px] text-black font-bold">80/100</p>
->>>>>>> a8cab4b6d361c448836065b1aaa3711f327f492c
+              <p className='font-Roboto text-[17px] text-[#98A0A4] '>
+                Passing Grade
+              </p>
+              <p className='font-Nunito text-[29px] text-black font-bold'>
+                80/100
+              </p>
             </div>
           </div>
-          <div className="flex flex-row font-Nunito mt-9 gap-7">
-            <div className="text-black bg-white shadow-[2px_3px_7px_rgba(0,0,0,0.15)] rounded-[24px] p-4">
-              <p className="text-5xl font-bold">15/20</p>
-              <p className="text-2xl">Right answers</p>
+          <div className='flex flex-row font-Nunito mt-9 gap-7'>
+            <div className='text-black bg-white shadow-[2px_3px_7px_rgba(0,0,0,0.15)] rounded-[24px] p-4'>
+              <p className='text-5xl font-bold'>15/20</p>
+              <p className='text-2xl'>Right answers</p>
             </div>
-            <div className="text-white bg-accent1 shadow-[2px_3px_7px_rgba(0,0,0,0.15)] rounded-[24px] py-4 px-7">
-              <p className="text-5xl font-bold">B+</p>
-              <p className="text-2xl font-bold">Grade</p>
+            <div className='text-white bg-accent1 shadow-[2px_3px_7px_rgba(0,0,0,0.15)] rounded-[24px] py-4 px-7'>
+              <p className='text-5xl font-bold'>B+</p>
+              <p className='text-2xl font-bold'>Grade</p>
             </div>
           </div>
         </div>
