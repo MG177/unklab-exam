f22c610ffb380ef36ee7c76919ab839f90c57f46
chaddjong <richardicad85@gmail.com>
2023-03-31 00:30:53 +0800

    update score page & score countdown page

--- my-app/src/components/TimerSmall.jsx
+++ my-app/src/components/TimerSmall.jsx
@@ -4,7 +4,7 @@ import Clock from "../image/clock_icon.svg";
 export default function TimerSmall() {
   return (
     <div className="flex flex-row justify-center items-center w-[254.03px] h-[61px] bg-white gap-[10px] mt-[41.5px] mb-[41.5px] mr-[120px] rounded-[24px] shadow-lg">
-      <img src={Clock} alt="" className="w-[35.03px] h-[35.03px]"/>
+      <img src={Clock} alt="" className="w-[35.03px] h-[35.03px] animate-bounce duration-[1ms]"/>
       <p className="text-accent2 font-bold font-nunito text-[41px]">90:59</p>
     </div>
   );
--- my-app/src/page/ScoreCountdown.jsx
+++ my-app/src/page/ScoreCountdown.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 import Header from "../components/Header";
 import Timer from "../components/Timer";
-import Warning from "../components/Warning";
+// import Warning from "../components/Warning";
 
 export default function ScoreCountdown() {
   return (
-<<<<<<< HEAD
-    <div className="w-full min-h-screen flex flex-col bg-[#FCF9FF]">
-=======
-    <div className="w-full h-screen flex flex-col bg-[#FCF9FF]">
->>>>>>> a8cab4b6d361c448836065b1aaa3711f327f492c
+    <div className="w-full h-screen justify-center items-center flex flex-col bg-[#FCF9FF]">
       <Header />
       {/* <div className="flex flex-row justify-end">
         <Warning />
       </div> */}
-      {/* <div className="flex flex-row justify-center min-h-max"> */}
-        <div className="bg-white wp-[50px] flex flex-col justify-center items-center gap-[28px] rounded-[24px] shadow-[0_5.9px_30px_rgba(0,0,0,0.58)]">
-          <Timer />
+      <div className="flex flex-row justify-center min-h-max">
+        <div className="bg-white p-[50px] flex flex-col justify-center items-center gap-[28px] rounded-[24px] shadow-[0_5px_25px_rgba(0,0,0,0.2)]">
+          <div className="shadow-[2px_3px_7px_rgba(0,0,0,0.15)] rounded-[24px]">
+            <Timer />
+          </div>
           <p className="font-[nunito] font-bold text-black text-[35px] w-[471px] text-center">
             Wait until the exam time is over to see your score.
           </p>
         </div>
-<<<<<<< HEAD
       </div>
-=======
-      {/* </div> */}
->>>>>>> a8cab4b6d361c448836065b1aaa3711f327f492c
     </div>
   );
 }
