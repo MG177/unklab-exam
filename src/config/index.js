91177eeb0c8eac2be72ec9d9aae821ec053ba93e
MG177 <mg.terok77@gmail.com>
2023-04-18 06:47:09 +0800

    change baseurl to deployment

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://unklab-exam-api.vercel.app'
   // baseURL: 'http://localhost:8080'
   // baseURL: 'https://ed3f-36-85-216-191.ngrok-free.app'
 });
