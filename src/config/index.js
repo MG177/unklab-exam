430de3344b4ddbb663bfd0ee2ada46b0d476de40
MG177 <mg.terok77@gmail.com>
2023-04-18 07:10:33 +0800

    remove http in url

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://unklab-exam-api.vercel.app'
+  baseURL: 'unklab-exam-api.vercel.app'
   // baseURL: 'http://localhost:8080'
   // baseURL: 'https://ed3f-36-85-216-191.ngrok-free.app'
 });
