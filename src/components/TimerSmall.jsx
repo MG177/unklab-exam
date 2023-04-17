ec5c3dc9ad11b6280f5cb2ce3ed70c1c6a0387af
Saylendra Yasin <saylendrayasin@gmail.com>
2023-04-17 10:24:49 +0800

    update: - button logout - add timer - change port for hit api

--- src/components/TimerSmall.jsx
+++ src/components/TimerSmall.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-// import Clock from "../image/clock_icon.svg";
+import React, { useState, useEffect } from 'react';
 
 export default function TimerSmall() {
+  const [timeRemaining, setTimeRemaining] = useState(15 * 60);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      window.location.href = '/score';
+    }
+  }, [timeRemaining]);
+
+  const minutes = Math.floor(timeRemaining / 60)
+    .toString()
+    .padStart(2, '0');
+  const seconds = (timeRemaining % 60).toString().padStart(2, '0');
+
   return (
     <div className="flex flex-row justify-center items-center w-[204.5px] h-[61px] bg-white gap-[10px] mt-[41.5px] mb-[41px] mr-[120px] px-[14px] py-[20px] rounded-[24px] shadow-[2px_3px_7px_0px_rgba(0,0,0,0.15)]">
-      {/* <img
-        src={Clock}
-        alt=""
-        className="w-[35.03px] h-[35.03px] animate-bounce duration-[1ms]"
-      /> */}
-      <p className="text-accent2 font-bold font-nunito text-[41px]">01:90:59</p>
+      <p className="text-accent2 font-bold font-nunito text-[41px]">{`${minutes}:${seconds}`}</p>
     </div>
   );
 }
--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -11,52 +11,57 @@ export default function Sidebar() {
   const [exams, setExams] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/exam',{
-      headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG5kb2UiLCJzdWIiOiIxMjMiLCJyb2xlIjpbImFkbWluIl0sImlhdCI6MTY4MTU5MTYxMCwiZXhwIjoxNjgxNjc4MDEwfQ.pQn9JHFNo60bOQQboL_V2Jo5frprWUznA8KXtItryBM'
-      }
-    })
-      .then(response => {
+    axios
+      .get('http://localhost:8080/exam', {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG5kb2UiLCJzdWIiOiIxMjMiLCJyb2xlIjpbImFkbWluIl0sImlhdCI6MTY4MTU5MTYxMCwiZXhwIjoxNjgxNjc4MDEwfQ.pQn9JHFNo60bOQQboL_V2Jo5frprWUznA8KXtItryBM',
+        },
+      })
+      .then((response) => {
         setExams(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
-  
+
   return (
-    <div className='flex flex-col h-screen min-w-fit bg-white shadow-right z-20'>
-      <div className='flex items-center justify-start p-3 h-16'>
+    <div className="flex flex-col h-screen min-w-fit bg-white shadow-right z-20">
+      <div className="flex items-center justify-start p-3 h-16">
         {isHidden ? (
-          <h1 className='font-bold text-3xl font-Nunito text-[29px] text-accent1'>
-            Unklab <span className='text-black'>Exams</span>
+          <h1 className="font-bold text-3xl font-Nunito text-[29px] text-accent1">
+            Unklab <span className="text-black">Exams</span>
           </h1>
         ) : (
-          <h1 className='font-bold text-3xl font-Nunito text-[29px] text-accent1'>
-            U<span className='text-black'>E</span>
+          <h1 className="font-bold text-3xl font-Nunito text-[29px] text-accent1">
+            U<span className="text-black">E</span>
           </h1>
         )}
       </div>
-      <nav className='flex-1 px-4'>
-        <ul className='space-y-2 font-Nunito'>
-        {exams.map(exam => (
-          <li
-            key={exam.id}
-            className={`flex font-bold p-3 bg-accent2 text-white rounded-lg ${
-              !isHidden ? 'gap-0 justify-center' : 'gap-3'
-            }`}
-          >
-            <span className='flex justify-center items-center'>{exam.examName}</span>
-            {isHidden && <a href='/'>{exam.examName}</a>}
-          </li>
-        ))}
+      <nav className="flex-1 px-4">
+        <ul className="space-y-2 font-Nunito">
+          {exams.map((exam) => (
+            <li
+              key={exam.id}
+              className={`flex font-bold p-3 bg-accent2 text-white rounded-lg ${
+                !isHidden ? 'gap-0 justify-center' : 'gap-3'
+              }`}
+            >
+              <span className="flex justify-center items-center">
+                {exam.examName}
+              </span>
+              {isHidden && <a href="/">{exam.examName}</a>}
+            </li>
+          ))}
         </ul>
       </nav>
       <div className={`w-full flex p-5`}>
         <button
           onClick={toggleHidden}
-          className='text-white bg-accent2 w-8 h-8 rounded-full hover:bg-accent1'>
-          <i className='fa-solid fa-bars' />
+          className="text-white bg-accent2 w-8 h-8 rounded-full hover:bg-accent1"
+        >
+          <i className="fa-solid fa-bars" />
         </button>
       </div>
     </div>
