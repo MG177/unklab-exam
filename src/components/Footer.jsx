c1300960f760c1bbf181edd407cac209f428a5fc
chaddjong <richardicad85@gmail.com>
2023-04-20 12:26:32 +0800

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TimerSmall from './TimerSmall';
-import Arrow from '../image/arrow_right.svg';
+import Arrow from '../image/arrow_next.svg';
 
 export default function Footer({ question, setQuestion, questions, time }) {
   console.log('question', question);
@@ -32,7 +32,7 @@ export default function Footer({ question, setQuestion, questions, time }) {
           onClick={handleNext}
           className="bg-white w-[86px] h-[86px] flex items-center justify-center mr-[120px] mt-[29px] mb-[29px]"
         >
-          <img src={Arrow} alt="" className="w-[26.52px] h-[43px]" />
+          <img src={Arrow} alt="" className="w-[59px] h-[44px]" />
         </button>
       )}
     </div>
--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import check from '../image/check_small.svg';
+import React from "react";
+import check from "../image/check_small.svg";
 
 export default function Option({ active, option, handleAnswer, widthFit }) {
   return (
     <div
       onClick={() => handleAnswer(option)}
       className={` ${
-        !widthFit ? 'w-[586px]' : 'w-full'
+        !widthFit ? "w-[586px]" : "w-full"
       } gap-[18px]  flex rounded-[24px] px-[15px] py-[20px] hover:backdrop-brightness-95  shadow-[2px_3px_7px_0px_rgba(0,0,0,0.15)]   ${
-        active ? 'bg-accent1 shadow-md shadow-accent1 ' : 'bg-whitePlus'
-      }`}>
+        active
+          ? "bg-accent1 shadow-md shadow-accent1 "
+          : "bg-whitePlus"
+      }`}
+    >
       {active ? (
-        <img src={check} alt='' />
+        <img src={check} alt="" />
       ) : (
-        <div className='w-[29px] h-[29px] bg-white rounded-[50%] border'></div>
+        <div className="w-[29px] h-[29px] bg-white rounded-[50%] border"></div>
       )}
-      <p className={`${active ? 'text-white ' : ''} text-[20px]`}>{option}</p>
+      <p
+        className={`${
+          active ? "text-white" : ""
+        } text-[20px]`}
+      >
+        {option}
+      </p>
     </div>
   );
 }
