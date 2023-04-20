d02bbe37e2d4eb57e10d590ba1890131e28817f2
chaddjong <richardicad85@gmail.com>
2023-04-20 17:31:32 +0800

    fixing terms and conditions

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -9,7 +9,7 @@ export default function Option({ active, option, handleAnswer, widthFit }) {
         !widthFit ? "w-[586px]" : "w-full"
       } gap-[18px]  flex rounded-[24px] px-[15px] py-[20px] hover:backdrop-brightness-95  shadow-[2px_3px_7px_0px_rgba(0,0,0,0.15)]   ${
         active
-          ? "bg-accent1 shadow-md shadow-accent1 "
+          ? "bg-accent1 shadow-md shadow-accent1 transition ease-out duration-300"
           : "bg-whitePlus"
       }`}
     >
@@ -20,7 +20,7 @@ export default function Option({ active, option, handleAnswer, widthFit }) {
       )}
       <p
         className={`${
-          active ? "text-white" : ""
+          active ? "text-white transition ease-out duration-300" : ""
         } text-[20px]`}
       >
         {option}
--- src/page/login/Start.jsx
+++ src/page/login/Start.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext";
 import api from "../../config";
+import TermsConditions from "../../image/terms and conditions.svg"
 
 export default function Start() {
   const { user } = useContext(AuthContext);
@@ -18,78 +19,27 @@ export default function Start() {
   };
 
   return (
-    <div className="p-[54px] rounded-[12px] bg-[#FAFAFA] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.25)] flex flex-col justify-evenly items-center">
+    <div className="p-[54px] h-max rounded-[12px] bg-[#FAFAFA] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.25)] flex flex-col justify-evenly items-center">
       <button
         type="button"
         className="py-4 rounded-full bg-[#ff032d] text-[#FAFAFA] font-semibold text-lg md:text-[24px] opacity-10 absolute top-0 w-2 h-2 left-390 right-0"
         onClick={logout}
       ></button>
-      <div className="w-[584px] h-[72px] top-[54px] left-[55.63px] flex flex-col justify-center items-center text-center">
-        <div className="flex">
-          <h1 className="font-bold font-Nunito text-[29px] text-black">
-            Welcome to our Software Exam Test
-            <span className="text-accent2">!</span>
-          </h1>
-        </div>
+      <div className="font-Nunito text-[29px] font-bold text-black">
+        <p>Welcome to the English Exam Test<span className="text-accent2">!</span></p>
       </div>
-      <div className="w-[581px] h-auto p-2 text-[14px] font-montserrat font-normal leading-[17px] flex flex-col justify-start">
-        <p className="">
-          Before taking the test, please carefully read and agree to the
-          following terms and conditions:
-        </p>
-        <br />
-        <p className="">
-          1. Test Format: The test will consist of multiple-choice questions
-          with some media like image and audio
-        </p>
-        <p>
-          2. Test Rules: You must adhere to the following rules while taking the
-          test:
-        </p>
-        <ol>
-          <li>
-            -You must not cheat or use any unauthorized resources during the
-            test.
-          </li>
-          <li>
-            -You must not share any information about the test or its contents
-            with anyone else.
-          </li>
-          <li>
-            -You must not attempt to record, copy, or reproduce any part of the
-            test.
-          </li>
-          <li>
-            -You must not disrupt or interfere with the testing environment or
-            other test-takers.
-          </li>
-        </ol>
-        <p>
-          3. Test Results: Your test results will be provided to you immediately
-          after completing the test, unless otherwise specified.
-        </p>
-        <p>
-          4. Intellectual Property: All intellectual property related to the
-          test, including but not limited to the test questions, coding
-          exercises, and any accompanying materials, are the property of the
-          test provider and may not be reproduced, distributed, or used without
-          permission.
-        </p>
-        <p>
-          By taking the test, you agree to be bound by these terms and
-          conditions. If you do not agree to these terms and conditions, you may
-          not take the test.
-        </p>
+      <div className="mt-[31px]">
+        <img src={TermsConditions} alt=""/>
       </div>
-      <div className="flex flex-col items-center justify-between h-7">
-        <form action="" className="flex gap-1">
+      <div className="flex flex-col items-center justify-between h-7 mt-[31.5px] mb-[54px]">
+        <form action="" className="flex gap-1 mb-[14.5px]">
           <input
             type="checkbox"
             id="agree"
             name="agreement"
             value={isChecked}
             onChange={handleChange}
-            className="w-[17.91px] h-[17.91px] rounded-full"
+            className="w-[17.91px] h-[17.91px] rounded-full focus:ring-white focus:checked:bg-accent2 checked:bg-accent2 checked:hover:bg-accent2"
           />
           <label
             htmlFor="agree"
@@ -98,7 +48,7 @@ export default function Start() {
             I agree to the Software Exam Test terms and conditions.
           </label>
         </form>
-        <Link to={"/exam/" + JSON.parse(localStorage.getItem("examId"))}>
+        <Link to={"/exam/" + JSON.parse(localStorage.getItem("examId"))} className="mb-[54px]">
           <button
             disabled={!isChecked}
             className="font-Nunito font-bold text-2xl py-[14px] px-[211px] rounded-[34px] disabled:text-black disabled:bg-[#E0E0E0] enabled:text-white enabled:bg-accent1"
