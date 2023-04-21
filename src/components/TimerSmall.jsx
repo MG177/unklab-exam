d1e3c3ceda70ba712c76d046be54efa20b50cc93
willzer0 <pembunuh4444@gmail.com>
2023-04-21 08:44:53 +0700

    footer error

--- src/components/TimerSmall.jsx
+++ src/components/TimerSmall.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { isValidDateValue } from "@testing-library/user-event/dist/utils";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function TimerSmall({ time }) {
+export default function TimerSmall({ time, classTime }) {
   const [timeRemaining, setTimeRemaining] = useState(0);
   const navigate = useNavigate();
 
@@ -14,7 +15,7 @@ export default function TimerSmall({ time }) {
       setTimeRemaining((prevTimeRemaining) => {
         if (prevTimeRemaining <= 1) {
           clearInterval(intervalId);
-          navigate('/score');
+          navigate("/score");
         } else {
           return prevTimeRemaining - 1;
         }
@@ -34,8 +35,19 @@ export default function TimerSmall({ time }) {
   const minutesStr = minutes.toString().length === 1 ? `0${minutes}` : minutes;
   // const secondsStr = seconds.toString().length === 1 ? `0${seconds}` : seconds;
 
+  const validateUrlPath = () => {
+    const rootExamPath = "/waiting";
+    if (rootExamPath) {
+      return "hidden";
+    } else if (!rootExamPath) {
+      return "flex flex-row justify-center items-center w-[204.5px] h-[61px] bg-white gap-[10px] mt-[41.5px] mb-[41px] mr-[120px] px-[14px] py-[20px] rounded-[24px] shadow-[2px_3px_7px_0px_rgba(0,0,0,0.15)]";
+    }
+  };
+  const isValidUrlPath = validateUrlPath();
+
   return (
-    <div className="flex flex-row justify-center items-center w-[204.5px] h-[61px] bg-white gap-[10px] mt-[41.5px] mb-[41px] mr-[120px] px-[14px] py-[20px] rounded-[24px] shadow-[2px_3px_7px_0px_rgba(0,0,0,0.15)]">
+    <div className={isValidUrlPath}>
+      {/* // <div className={classTime}> */}
       <p className="text-accent2 font-bold font-nunito text-[41px]">
         {/* {hours > 0 ? `${hoursStr} : ` : ''}
         {`${minutesStr} : ${secondsStr}`} */}
