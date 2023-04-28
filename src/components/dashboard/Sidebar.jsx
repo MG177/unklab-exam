d89869cf33a7c85850fd6f6c4e5cabbcddaf1ff3
MG177 <mg.terok77@gmail.com>
2023-04-28 08:21:48 +0800

    eslint

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import api from '../../config';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import api from "../../config";
 
 export default function Sidebar({
   examlist,
@@ -11,7 +11,7 @@ export default function Sidebar({
   examActive,
   user,
   fetchTime,
-  setToken
+  setToken,
 }) {
   const [isHidden, setIsHidden] = useState(false);
 
@@ -24,9 +24,9 @@ export default function Sidebar({
       .get(`/exam`, {
         headers: {
           Authorization: `Bearer ${JSON.parse(
-            localStorage.getItem('access_token')
-          )}`
-        }
+            localStorage.getItem("access_token")
+          )}`,
+        },
       })
       .then((response) => {
         fetchTime(response.data[0]._id);
@@ -39,14 +39,15 @@ export default function Sidebar({
   }, []);
 
   const getDbQuestions = async (id) => {
+    console.log("get db questions");
     try {
       await api
         .get(`/questions/exam/${id}`, {
           headers: {
             Authorization: `Bearer ${JSON.parse(
-              localStorage.getItem('access_token')
-            )}`
-          }
+              localStorage.getItem("access_token")
+            )}`,
+          },
         })
         .then((response) => {
           setDbQuestions(response.data);
@@ -55,6 +56,7 @@ export default function Sidebar({
     } catch (error) {
       console.log(error);
     }
+    console.log("end: get db questions");
   };
 
   const handleSidebarButton = (exam) => {
@@ -71,15 +73,15 @@ export default function Sidebar({
   };
 
   return (
-    <div className='z-20 flex flex-col h-screen bg-whitePlus min-w-fit shadow-right'>
-      <div className='flex items-center justify-center h-16 p-3'>
+    <div className="z-20 flex flex-col h-screen bg-whitePlus min-w-fit shadow-right">
+      <div className="flex items-center justify-center h-16 p-3">
         {isHidden ? (
-          <h1 className='font-bold text-3xl font-Nunito text-[29px] text-accent1'>
-            Unklab <span className='text-black'>Exams</span>
+          <h1 className="font-bold text-3xl font-Nunito text-[29px] text-accent1">
+            Unklab <span className="text-black">Exams</span>
           </h1>
         ) : (
-          <h1 className='font-bold text-3xl font-Nunito text-center text-[29px] text-accent1'>
-            U<span className='text-black'>E</span>
+          <h1 className="font-bold text-3xl font-Nunito text-center text-[29px] text-accent1">
+            U<span className="text-black">E</span>
           </h1>
         )}
         {isHidden && (
@@ -91,7 +93,7 @@ export default function Sidebar({
           </button>
         )}
       </div>
-      <nav className='flex-1 px-4'>
+      <nav className="flex-1 px-4">
         <ul className={`space-y-2 font-Nunito text-gray bg-white`}>
           {examlist.map((exam, index) => (
             <button
@@ -100,20 +102,22 @@ export default function Sidebar({
                 handleSidebarButton(exam);
               }}
               className={`cursor-pointer flex flex-row items-center font-bold w-full shadow-right rounded-lg p-3 ${
-                !isHidden ? 'gap-0 justify-center' : 'gap-4 px-4 justify-start'
-              } ${examActive === exam._id && `bg-accent2 text-white`}`}>
-              <a href='.'>{index + 1}</a>
-              <a href='.'>{isHidden && exam.examName}</a>
+                !isHidden ? "gap-0 justify-center" : "gap-4 px-4 justify-start"
+              } ${examActive === exam._id && `bg-accent2 text-white`}`}
+            >
+              <a href=".">{index + 1}</a>
+              <a href=".">{isHidden && exam.examName}</a>
             </button>
           ))}
         </ul>
       </nav>
-      <div className='flex justify-end w-full p-5'>
+      <div className="flex justify-end w-full p-5">
         <button
           onClick={toggleHidden}
-          className='w-8 h-8 text-white rounded-full bg-accent2'>
+          className="w-8 h-8 text-white rounded-full bg-accent2"
+        >
           <i
-            className={isHidden ? 'pi pi-chevron-left' : 'pi pi-chevron-right'}
+            className={isHidden ? "pi pi-chevron-left" : "pi pi-chevron-right"}
           />
         </button>
       </div>
