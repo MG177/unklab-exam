3b8ba0dabb48ea02f5cf81e3e09f5465a702f2a4
MG177 <mg.terok77@gmail.com>
2023-04-28 10:19:45 +0800

    ganti end point

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: 'https://unklab-exam-api.vercel.app'
-  baseURL: "//localhost:8080",
-  // baseURL: "https://unklab-exam-api-nqbkioe7dq-as.a.run.app/",
+  // baseURL: "//localhost:8080",
+  baseURL: "https://unklab-exam-api-nqbkioe7dq-as.a.run.app/",
 });
 
 export default api;
