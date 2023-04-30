78a52cecd2f93befb14b2f80a3131a280a41679f
Sitou <yehezkielowen0901@gmail.com>
2023-05-01 02:52:48 +0800

    amend sidebar using params

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./page/login/Login";
-import Getstarted from "./page/login/Getstarted";
-import ScoreCountdown from "./page/score/ScoreCountdown";
-import Exam from "./page/Exam";
-import Score from "./page/score/Score";
-import Testing from "./page/testing";
-import PageDashboard from "./page/dashboard/PageDashboard";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './page/login/Login';
+import Getstarted from './page/login/Getstarted';
+import ScoreCountdown from './page/score/ScoreCountdown';
+import Exam from './page/Exam';
+import Score from './page/score/Score';
+import Testing from './page/testing';
+import PageDashboard from './page/dashboard/PageDashboard';
 
-import ProtectedRoute from "./utils/ProtectedRoute";
-import AuthContextLayout from "./contexts/AuthContextLayout";
-import QuestionContextLayout from "./contexts/QuestionContextLayout";
+import ProtectedRoute from './utils/ProtectedRoute';
+import AuthContextLayout from './contexts/AuthContextLayout';
+import QuestionContextLayout from './contexts/QuestionContextLayout';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename='/'>
       <Routes>
         <Route element={<AuthContextLayout />}>
-          <Route path="/" element={<Login />} />
+          <Route path='/' element={<Login />} />
           <Route
-            path="/started"
+            path='/started'
             element={
               <ProtectedRoute>
                 <Getstarted />
@@ -27,7 +27,7 @@ function App() {
             }
           />
           <Route
-            path="/waiting"
+            path='/waiting'
             element={
               <ProtectedRoute>
                 <ScoreCountdown />
@@ -35,7 +35,7 @@ function App() {
             }
           />
           <Route
-            path="/exam/:examId"
+            path='/exam/:examId'
             element={
               <ProtectedRoute>
                 <Exam />
@@ -43,7 +43,7 @@ function App() {
             }
           />
           <Route
-            path="/score"
+            path='/score'
             element={
               <ProtectedRoute>
                 <Score />
@@ -51,7 +51,7 @@ function App() {
             }
           />
           <Route
-            path="/testing"
+            path='/testing'
             element={
               <ProtectedRoute>
                 <Testing />
@@ -60,7 +60,7 @@ function App() {
           />
           <Route element={<QuestionContextLayout />}>
             <Route
-              path="/dashboard"
+              path='/dashboard/:examId'
               element={
                 <ProtectedRoute>
                   <PageDashboard />
@@ -69,7 +69,7 @@ function App() {
             />
           </Route>
           {/* <Route path='/login' element={<Login />} /> */}
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path='*' element={<Navigate to='/' />} />
         </Route>
       </Routes>
     </BrowserRouter>
