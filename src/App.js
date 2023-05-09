5feec844e0db5504ae0aef12670832bfe13e7bdf
Jennifer Anggie Muntuan <jenniferanggiemuntuan@Jennifers-MacBook-Air.local>
2023-05-09 12:58:04 +0800

    cacel redirect

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
             path="/score"
             element={
               <ProtectedRoute>
-                <Score />
+                <Login />
               </ProtectedRoute>
             }
           />
