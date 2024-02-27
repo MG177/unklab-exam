a09f0448c56affd3d7f6249f2fc210b343593847
MG <mg.terok77@gmail.com>
2024-02-27 17:42:14 +0800

    update baseURL

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://kep-unklab-exam-api-x5rigwkaha-as.a.run.app',
+  // baseURL: 'https://kep-unklab-exam-api-x5rigwkaha-as.a.run.app',
   // baseURL: '//localhost:8080',
+  // baseURL: 'http://localhost:8080',
+  baseURL: process.env.BACKEND_URI || '//localhost:8080',
 });
 
 export default api;
